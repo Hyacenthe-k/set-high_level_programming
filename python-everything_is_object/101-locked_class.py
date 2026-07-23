ae9264a3575fd8ca79b21c4a453c264e180cb039
karinda hyacenthe <karindahyacenthe1@gmail.com>
2026-07-23 22:13:31 +0200

    fixedf doccumentation

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a LockedClass."""
+
+
 class LockedClass:
+    """A class that prevents dynamic creation of attributes
+
+    except if the attribute is named 'first_name'.
+    """
     __slots__ = ['first_name']
