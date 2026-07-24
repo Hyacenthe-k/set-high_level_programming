c005ee7a69b7101afbf487154497625a7f88c9e3
karinda hyacenthe <karindahyacenthe1@gmail.com>
2026-07-24 22:52:47 +0200

    fixed line 19

--- python-if_else_loops_functions/13-insert_number.c
+++ python-if_else_loops_functions/13-insert_number.c
@@ -16,7 +16,7 @@ listint_t *insert_node(listint_t **head, int number)
     new_node->n = number;
     new_node->next = NULL;
 
-    / Insert at the beginning or into an empty list
+ /* Insert at the beginning or into an empty list
     if (*head == NULL || (*head)->n >= number)
     {
         new_node->next = *head;
