b7fcb5fb78bce2ff7f8af29a3461f73ae098efdb
Hyacenthe-k <karindahyacenthe1@gmail.com>
2026-07-27 04:56:58 -0700

    fixed task 8 concat

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word[0:26] + word[31:36] + word[-6:])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with clear syntax"
+str = str[39:67] + str[106:112] + str[:6]
+print(str)
