269f71480272909d6c5db4b40de03387d50b373d
Hyacenthe-k <karindahyacenthe1@gmail.com>
2026-07-29 13:32:04 -0700

    create 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines a text file writing function.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF-8 text file and returns characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
