d6a51f8f1fe70eea45a9ef3e3871da42119306e6
Hyacenthe-k <karindahyacenthe1@gmail.com>
2026-08-16 12:45:47 -0700

    Fix pycodestyle line lengths and repo URL parameter order

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module to list recent 10 commits of a GitHub repository by owner using requests.
+Module to list 10 recent commits of a GitHub repo by owner.
 """
 import requests
 import sys
@@ -9,14 +9,17 @@ import sys
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repo_name)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        repo_name, owner_name
+    )
 
     r = requests.get(url)
-    try:
-        commits = r.json()
+    commits = r.json()
+
+    if isinstance(commits, list):
         for commit in commits[:10]:
             sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
+            author_name = commit.get('commit', {}).get(
+                'author', {}
+            ).get('name')
             print("{}: {}".format(sha, author_name))
-    except Exception:
-        pass
