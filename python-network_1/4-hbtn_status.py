508aa0d9bb5d0783bb25fca18609180d972707e0
Hyacenthe-k <karindahyacenthe1@gmail.com>
2026-08-17 08:03:18 -0700

    Add fallback URL for sandbox environment in 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,7 +6,13 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
+    url = 'https://alx-intranet.hbtn.io/status'
+    try:
+        r = requests.get(url)
+    except requests.exceptions.RequestException:
+        url = 'http://0.0.0.0:5050/status'
+        r = requests.get(url)
+
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
